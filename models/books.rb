aa0047d8f5f1aeaed93a23a8d412b54a18e538f6
Oceane <oceanelonneux@gmail.com>
2018-08-26 21:55:01 +0100

    Worked on database

--- models/books.rb
+++ models/books.rb
@@ -3,6 +3,7 @@ class Book
   attr_accessor(:id, :title, :book_body, :genre)
 
   def self.open_connection
+    # connect to database
     conn = PG.connect(dbname: "books")
   end
 
@@ -12,4 +13,21 @@ class Book
     books_result = conn.exec(sql)
     book = self.hydrate(books_result[0])
   end
+
+  def self.destroy(id)
+    conn = self.open_connection
+    sql = "DELETE FROM book WHERE id = #{id}"
+    conn.exec(sql)
+  end
+
+  def self.hydrate(book_data)
+    book = Book.new
+    book.id = book_data['id']
+    book.title = book_data['title']
+    book.book_body = book_data['book_body']
+    book.genre = book_data['genre']
+  end
+
+
 end
+# end of class Book
---
+++ seed.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  book_body TEXT
+  genre VARCHAR(35)
+
+);
+
+INSERT INTO book (title , book_body) VALUES ('Book 0' , 'Body text 0');
+INSERT INTO book (title , book_body) VALUES ('Book 1' , 'Body text 1');
+INSERT INTO book (title , book_body) VALUES ('Book 2' , 'Body text 2');
+INSERT INTO book (title , book_body) VALUES ('Book 3' , 'Body text 3');
